51987dc86860a805bc9771ac9ab43c4f93ce956d
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-05-18 12:41:19 -0400

    Contact book gui incorporated.

--- contacts/views.py
+++ contacts/views.py
@@ -2,6 +2,10 @@ from PyQt5.QtWidgets import (
     QHBoxLayout,
     QMainWindow,
     QWidget,
+    QAbstractItemView,
+    QPushButton,
+    QTableView,
+    QVBoxLayout
 )
 
 ## Set parameters to main window
@@ -14,3 +18,24 @@ class Window(QMainWindow):
         self.setCentralWidget(self.centralWidget)
         self.layout = QHBoxLayout()
         self.centralWidget.setLayout(self.layout)
+        # main window gui addition;
+        self.setupUI()
+    
+    #setting up main window 
+    def setupUI(self):
+        # create table view with abstract item view so complete row selected
+        self.table = QTableView()
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.resizeColumnsToContents()
+        # add buttons
+        self.addButton = QPushButton("Add...")
+        self.deleteButton = QPushButton("Delete")
+        self.clearAllButton = QPushButton("Clear All")
+        # add vertical display w buttons
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addStretch()
+        layout.addWidget(self.clearAllButton)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
